b6df463f66a1d112d32bad718cff7dbc317b8f52
VitaliiPodshibyakin <vitalikpod@yandex.ru>
2022-01-29 17:13:52 +0300

    Setup logic

--- HW-2.3/LoginViewController.swift
+++ HW-2.3/LoginViewController.swift
@@ -8,22 +8,18 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-
+    
+    @IBOutlet var loginTextField: UILabel!
+    
+    var loginText: String!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        loginTextField.text = loginText
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func logOutButton() {
+        dismiss(animated: true) {
+        }
     }
-    */
-
 }
--- HW-2.3/ViewController.swift
+++ HW-2.3/ViewController.swift
@@ -8,12 +8,56 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var userNameTextField: UITextField!
+    @IBOutlet var passwordTextfield: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
+    @IBAction func logInButton() {
+        if userNameTextField.text == "user", passwordTextfield.text == "password" {
+        } else {
+            showAlert(with: "Error", and: "Invalid login or password")
+            passwordTextfield.text = ""
+        }
+        }
 
+    @IBAction func forgotLoginButton() {
+        showAlert(with: "Oops!", and: "Your login is 'user'")
+        }
+    
+    @IBAction func forgotPassword() {
+        showAlert(with: "Oops!", and: "Your login is 'password'")
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let loginViewController = segue.destination as? LoginViewController else {return}
+        loginViewController.loginText = "Hello, " + userNameTextField.text! + "!"
+    }
+    
+    @IBAction func unwind(for segue: UIStoryboardSegue) {
+        passwordTextfield.text = ""
+        userNameTextField.text = ""
+    }
 }
+
+
+// MARK: - Alerts
+extension ViewController {
+    private func showAlert(with title: String, and massage: String) {
+        let alert = UIAlertController(title: title, message: massage, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default)
+        alert.addAction(okAction)
+        present(alert, animated: true)
+    }
+}
+// MARK: - Touches
+extension ViewController {
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super .touchesBegan(touches, with: event)
+    }
+}
+
 
